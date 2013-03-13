628dda006c0d5eb52b53df49690f716d5544848f
Dan Harper <intouch@danharper.me>
2013-03-13 20:57:03 +0000

    a start

--- src/danharper/DTI/DTI.php
+++ src/danharper/DTI/DTI.php
@@ -1,9 +1,32 @@
 <?php namespace danharper\DTI;
 
+use DateTime;
+use DateInterval;
+
 class DTI {
 
 	public function __construct()
 	{
+	}
+
+	public function parse($input, $default_datetime = null)
+	{
+		if ( ! $default_datetime instanceof DateTime)
+		{
+			$default_datetime = new DateTime($default_datetime);
+		}
+
+		if (preg_match('/^P/', $input))
+		{
+			$output = clone $default_datetime;
+			$output->sub(new DateInterval($input));
+		}
+		else
+		{
+			$output = new DateTime($input);
+		}
+
+		return array($output, $default_datetime);
 	}
 
 }
--- tests/DTITest.php
+++ tests/DTITest.php
@@ -1,7 +1,6 @@
 <?php
 
 use \danharper\DTI\DTI;
-use \Mockery as m;
 
 class DTITest extends PHPUnit_Framework_TestCase {
 
@@ -12,7 +11,6 @@ class DTITest extends PHPUnit_Framework_TestCase {
 
 	public function tearDown()
 	{
-		m::close();
 	}
 
 	public function testStart()
@@ -20,5 +18,41 @@ class DTITest extends PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf('\danharper\DTI\DTI', $this->dti);
 	}
 
+	public function testParseReturnsTwoDateTimeObjects()
+	{
+		list($from, $to) = $this->dti->parse('2007-03-01T13:00:00Z');
+		$this->assertInstanceOf('DateTime', $from);
+		$this->assertInstanceOf('DateTime', $to);
+	}
+
+	public function testParseSingleDateTime()
+	{
+		list($from, $to) = $this->dti->parse('2007-03-01T13:00:00Z');
+		$this->assertEquals(new DateTime('2007-03-01T13:00:00Z'), $from);
+	}
+
+	public function testParseAcceptsDefaultDatetime()
+	{
+		$time = new DateTime;
+		list($from, $to) = $this->dti->parse('2007-03-01T13:00:00Z', $time);
+		$this->assertEquals($time, $to);
+	}
+
+	public function testParseSingleDuration()
+	{
+		$baseTime = time();
+
+		$datetime = new DateTime;
+		$datetime->setTimestamp($baseTime);
+
+		$expected = new DateTime;
+		$expected->setTimestamp($baseTime);
+
+		list($from, $to) = $this->dti->parse('P1Y2M10DT2H30M', $datetime);
+		$expected->sub(new DateInterval('P1Y2M10DT2H30M'));
+
+		$this->assertEquals($from, $expected);
+		$this->assertEquals($to, $datetime);
+	}
 
 }
